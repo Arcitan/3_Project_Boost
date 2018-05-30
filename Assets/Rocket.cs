77e7e1b5cb8a466d3b1d2263f61ef1280a7fba9c
Franklin <franklinzgma@gmail.com>
2018-05-30 13:14:19 -0500

    Added rocket audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rigidBody; 
+    Rigidbody rigidBody;
+    AudioSource audioSource; 
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -23,6 +25,14 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space)) 
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // so it doesn't layer audio 
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         // Rotation inputs
